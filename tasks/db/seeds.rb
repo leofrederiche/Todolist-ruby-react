6e64be8abf5529fc264b10bde0aaf2ea7f9a95bf
Leonardo Camp <leofrederiche@gmail.com>
2019-07-14 13:32:44 -0300

    First commit

---
+++ tasks/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: 'Criar um icone/logo para o Sistema', done: true)
+Task.create(title: 'Definir o nome do sistema', done: false)
+Task.create(title: 'Definir Planos e Oferecimentos', done: false)
+Task.create(title: 'Criar Mokup', done: false)
+Task.create(title: 'Realizar pesquisa virtual', done: false)
+Task.create(title: 'Realizar pesquisa de campo', done: false)
+Task.create(title: 'Criar estrutura do sistema', done: false)
